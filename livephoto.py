4a775b0b27794d4165bf3e83f792641a2171ed12
kar7ik <kartik.arora93@gmail.com>
2020-07-01 16:50:49 +0530

    gif creator with raw images retaining at a fixed path

--- livephoto.py
+++ livephoto.py
@@ -6,25 +6,31 @@ import glob
 import imageio
 import os
 
+DIR = "/home/kar7ik/livephotoapp/livephotoapp/live-photos/live-photos_"
 
 livephoto = []
 
+curr_time = str(datetime.now())
+dirr = DIR+curr_time
+# creating new directory everytime with timestamp to retain raw images
+if not os.path.exists(dirr):
+    os.mkdir(dirr)
 
-for x in range(20):
+for x in range(15):
     image = pyautogui.screenshot()
 
 # convert RGB it to numpy array and BGR
-# so we can write it to the disk
+# write it to the disk
 
     image = cv2.cvtColor(np.array(image),
                          cv2.COLOR_RGB2BGR)
 
 # writing it to the disk using opencv
-    cv2.imwrite("/home/kar7ik/livephotoapp/livephotoapp/live-photos/IMG_" +
+    cv2.imwrite(DIR+curr_time+"/IMG_" +
                 str(datetime.now())+".png", image)
 
 livephoto = glob.glob(
-    "/home/kar7ik/livephotoapp/livephotoapp/live-photos/*.png")
+    DIR+curr_time+"/*.png")
 
 
 images = []
@@ -32,9 +38,7 @@ for photo in livephoto:
     images.append(imageio.imread(photo))
 imageio.mimsave(
     '/home/kar7ik/livephotoapp/livephotoapp/live-photo-gif/livephoto_' +
-    str(datetime.now())+'.gif', images, duration=0.2)
+    str(datetime.now())+'.gif', images, duration=0.25)
 
 
-for filename in glob.glob('/home/kar7ik/livephotoapp/livephotoapp/live-photos/*'):
-    os.remove(filename)
-    print("Done")
+print("Done")
